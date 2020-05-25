2093abf19e04d3478bfbb6cecdc3e7518c8f7c8d
unknown <ackitamura93@gmail.com>
2020-05-24 18:45:00 -0700

    Adding the election audit code.

---
+++ Python_practice.py
@@ -0,0 +1,6 @@
+
+
+
+voting_data = [{"county":"Arapahoe","registered_voters":422829},{"county":"Denver","registered_voters":463353}]
+for county, voters in voting_data:
+    print(f"{county} county has {voters} registered voters.")
