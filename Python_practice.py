2a8515bfaf8dfc7aa96afac0cbce4df361233c5d
akitamura <65060650+akitamura@users.noreply.github.com>
2020-05-29 09:44:53 -0700

    Delete Python_practice.py
    Removing for "not needed for challenge"

--- Python_practice.py
+++
@@ -1,6 +0,0 @@
-
-
-
-voting_data = [{"county":"Arapahoe","registered_voters":422829},{"county":"Denver","registered_voters":463353}]
-for county, voters in voting_data:
-    print(f"{county} county has {voters} registered voters.")
